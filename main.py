bd9221b93d134259ef6997c883a57edcaae00c0d
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-17 10:16:55 -0300

    Update main.py:
    - Use driver variable instead of browser
    - Print text of an element using CSS selector
    - Find and print number of <p> elements in the page
    - Use Keys.RETURN to submit the search form

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+# Inicializa o navegador Firefox
+driver = webdriver.Firefox()
 
-browser.get("https://nostarch.com/automatestuff2")
+# Abre a URL desejada
+driver.get("https://nostarch.com/automatestuff2")
 
-elem = browser.find_element_by_css_selector(
+# Encontra o elemento usando o seletor CSS
+elem2 = driver.find_element_by_css_selector(
+    "#edit-attributes-1 > div:nth-child(1) > label"
+)
+
+# Obtém o texto do elemento
+print(elem2.text)
+
+# Encontra o elemento de entrada de texto usando o seletor CSS
+elem = driver.find_element_by_css_selector(
     "div.logo-wrapper:nth-child(2) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
 )
 
-elems = browser.find_elements_by_css_selector("p")
+# Encontra todos os elementos <p> na página
+elems = driver.find_elements_by_css_selector("p")
 
-searchElem = browser.find_elements_by_selector(
+# Encontra o elemento de entrada de texto para pesquisa
+searchElem = driver.find_element_by_css_selector(
     "div.logo-wrapper:nth-child(2) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
 )
 
-searchElem.send_keys("Ola")
-searchElem.submit()
+# Insere texto no elemento de entrada de texto e envia o formulário
+searchElem.send_keys("Olá")
+searchElem.send_keys(Keys.RETURN)  # Envio do formulário pressionando a tecla Enter
 
-browser.quit()
+# Fecha o navegador
+driver.quit()
